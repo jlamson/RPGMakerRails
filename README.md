546c8b643bc6f0df3e034273160f25d163538c55
Joshua Lamson <jlamson@mymail.mines.edu>
2013-03-31 17:14:38 -0600

    New, blank application

--- README.md
+++
@@ -1,4 +0,0 @@
-RPGMakerRails
-=============
-
-This is a repo for my RPG system designer's back end. 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RPGMakerRails::Application.config.secret_token = '7ad55ce00c213302a884462b24760a19e35626b4efd1badd84efda057627c866cdd0b5e2af13b315c33a092517f8693c554361e86faaf4b97fa33f6ab18116eb'
