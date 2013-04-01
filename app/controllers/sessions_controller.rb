930ee20790e42354c72de333ed2e1b844b41c101
Joshua Lamson <jlamson@mymail.mines.edu>
2013-03-31 22:46:33 -0600

    Merged from branch 'user_login.' Users can register and login. Currently no access control is in place, but all actions with the session are in place. Squashed commit of the following:
    commit ab313263dc65ef5c0cb80869adc59a510d9f9ea1
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 22:37:32 2013 -0600
    
        commented out admin test (will add later)
    
    commit 99cc3967b8b5cc23cb3eddb9682c3fd080c2ae26
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 22:35:59 2013 -0600
    
        Updated session tests and user fixtures
    
    commit 06b8d4999e406e92ed20adbc90c5787240a527f5
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 22:23:49 2013 -0600
    
        Corrected bug when logging out (wrong http method)
    
    commit 83e272443b67c776f9d0b7d703f61c29f61f5994
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 22:17:42 2013 -0600
    
        Put "new" html in "create" html. Swapped content
    
    commit e1c20a9755e9095ee8d853f6be4cd2568eb9bc48
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 22:15:56 2013 -0600
    
        Modified Header for registration
    
    commit 412303ed527263d95904e57f462cc1b7549cc690
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 22:15:39 2013 -0600
    
        Generate links to login and register in application
    
    commit cd2be32cb13ffa44c212e050dd5612b906487754
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 22:05:51 2013 -0600
    
        Modified routes to have admin/index be default for admin, and capture sessions urls
    
    commit fdc39ab62a1604c46f5dac4bb08669e460a1aaf7
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 22:02:52 2013 -0600
    
        Made admin index show dummy page that says username
    
    commit 624496796bbc98af2811063346def8fe5d1f0f58
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 21:51:24 2013 -0600
    
        Added logout function (sessions#destroy) that redirects to Home with notice of logout
    
    commit cd61ae0435e0a5bf8f2dd39791350d29e749dd08
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 21:50:48 2013 -0600
    
        Added a notice hook to home page
    
    commit 0a6115b000fe8bbaa3fb66570f0de7dfb7b00813
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 21:48:48 2013 -0600
    
        Created a sessions/create layout (Login page)
    
    commit 197766c652e13894b0e53fcd147495db87c738f0
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 21:40:03 2013 -0600
    
        add session/create to redirect to admin if user exists or back to login if error
    
    commit bca3649e1083be5e6f886ab7628141ac8a07a14f
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 21:33:02 2013 -0600
    
        ran 'rails generate controller admin index'
    
    commit 16e88f5ea412b37459a0c0deaa8967020b2f32f5
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 21:31:55 2013 -0600
    
        Ran 'rails generate controller sessions new create destroy'

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if user = User.authenticate(params[:username], params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url
+    else
+      redirect_to login_url, alert: "Invalid username and/or password"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged Out"
+  end
+end
---
+++ test/functional/admin_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class AdminControllerTest < ActionController::TestCase
+  # TODO: write to to ensure page only works when logged in
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  # end
+
+end
