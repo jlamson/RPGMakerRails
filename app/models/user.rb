73020ef45e49dacbf7f7915e455d5e3fa935c7f9
Joshua Lamson <jlamson@mymail.mines.edu>
2013-03-31 20:23:31 -0600

    Added validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :hashed_password, :salt, :username
+  validates :username, presence: true, uniqueness: true
+  
+  validates :password, confirmation: true
+  attr_accessor :password_confirmation
+  attr_reader :password
+
+  validate :password_must_be_set
+
+  private def password_must_be_set
+    errors.add(:password, "Missing Password") unless hashed_password.present?
+  end
 end
