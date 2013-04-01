d94a35b5920c306db93687f5a34f4adb152e3bf3
Joshua Lamson <jlamson@mymail.mines.edu>
2013-04-01 00:42:39 -0600

    Added paperclip in Gemfile and environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ RPGMakerRails::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Include 
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
