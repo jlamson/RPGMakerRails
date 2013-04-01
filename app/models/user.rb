c68cda5fa9073eee745e188f9c61b44e24132f84
Joshua Lamson <jlamson@mymail.mines.edu>
2013-03-31 21:27:35 -0600

    Merged from branch 'user_init.' Users can now be created and viewed. (Squashed commit)
    commit 967da3e5bd9cd0b110225592d0bedd3f856cc623
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 21:25:44 2013 -0600
    
        Updated tests to (a) use a defined set of inputs, and (b) assert redirected to users_url rather than user/show
    
    commit afb635485ca1c3ddd5fcffc17c2a4f2283bb7669
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 21:17:12 2013 -0600
    
        Fixed issue with background color
    
    commit 486675979a8d54e41a71821d7a144cc5896041c6
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 21:10:39 2013 -0600
    
        Added attr_accessible for User attrs in form.html.erb
    
    commit 39723254ccdd64119b8f6c4f4822ce29c9183b17
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 21:10:13 2013 -0600
    
        Modified label in form
    
    commit f81e4706baac4928f24902ea59ab32e5fa1c0f47
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 21:06:07 2013 -0600
    
        Modified for for User (new)
    
    commit 22afb57207d11288c37addaf9d4019ee61241fc2
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 20:57:52 2013 -0600
    
        Added notice to Users index. Also removed columns in table for Hashed Password and Salt
    
    commit d7b0781354762d0903915bed21079fe8c0268fcd
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 20:48:10 2013 -0600
    
        Modified Users index to return Users sorted by name
    
    commit f4e11bfeb8045fed4207aa7b8db0018a10b29932
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 20:47:00 2013 -0600
    
        Removed redirect to show, and replaced with redirect to users_url with notice of action (create or update)
    
    commit 9b81d83eb98754d3eefea6e6599ec1d2badf5d43
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 20:41:01 2013 -0600
    
        Added authenticate method to User model
    
    commit 6f012d65f56d63fbc715ce3156b0aa382ab23560
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 20:38:40 2013 -0600
    
        added writer for password (virtual attribute) that also sets hashed_password to persist in db.
    
    commit 5e5b297cdf4eb8dd777cdd0082574dfcc6afcb54
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 20:36:01 2013 -0600
    
        Added a method to generate a random salt
    
    commit b662389dcb6bb84b10596701ea1b4c5e3ab683ff
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 20:33:43 2013 -0600
    
        Added encrypt password function to User
    
    commit 73020ef45e49dacbf7f7915e455d5e3fa935c7f9
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 20:23:31 2013 -0600
    
        Added validation to User model
    
    commit d76628a1d1113c9fd0f7457fac32b72eda76f002
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 20:17:52 2013 -0600
    
        Migrated User
    
    commit e398ebdffc8f83851faa7c26a40cabaa6d7de300
    Author: Joshua Lamson <jlamson@mymail.mines.edu>
    Date:   Sun Mar 31 20:17:02 2013 -0600
    
        Ran 'rails generate scaffold user …'

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ActiveRecord::Base
+  attr_accessible :username, :password, :password_confirmation
+  validates :username, presence: true, uniqueness: true
+  
+  validates :password, confirmation: true
+  attr_accessor :password_confirmation
+  attr_reader :password
+
+  validate :password_must_be_set
+
+  def User.authenticate(username, password) 
+    if user = find_by_username(username)
+      if user.hashed_password == encrypt_password(password, user.salt) 
+        user
+      end 
+    end
+  end
+
+  def password=(password)
+    @password = password
+
+    if password.present?
+      generate_salt
+      self.hashed_password = self.class.encrypt_password(password, salt)
+    end 
+  end
+
+  def User.encrypt_password(password, salt)
+    Digest::SHA2.hexdigest(password + "NaCl" + salt)
+  end
+
+  private 
+  def password_must_be_set
+    errors.add(:password, "Missing Password") unless hashed_password.present?
+  end
+
+  def generate_salt 
+    self.salt = self.object_id.to_s + rand.to_s
+  end
+end
