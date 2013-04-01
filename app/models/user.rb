918f7d34676faac5e81e33fd6e0343231fd58daa
Joshua Lamson <jlamson@mymail.mines.edu>
2013-04-01 00:52:00 -0600

    Added verification for pic attachment in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :username, :password, :password_confirmation
   validates :username, presence: true, uniqueness: true
   
+  has_attached_file :pic, styles: { medium: "300x300>", thumb; "100x100>" }
+
   validates :password, confirmation: true
   attr_accessor :password_confirmation
   attr_reader :password
