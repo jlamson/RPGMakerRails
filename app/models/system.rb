af7345969527c7b95a462352c084e5b9afac29de
Joshua Lamson <jlamson@mymail.mines.edu>
2013-04-12 20:49:20 -0600

    Modified more "public" => "is_public" and added links to a new system to index

--- app/models/system.rb
+++ app/models/system.rb
@@ -1,5 +1,5 @@
 class System < ActiveRecord::Base
-  attr_accessible :description, :name, :parent_id, :public, :user_id
+  attr_accessible :description, :name, :parent_id, :is_public, :user_id
   belongs_to :user
   belongs_to :system
   has_one :system
