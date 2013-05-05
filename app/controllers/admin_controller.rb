45a9bd7ebd8b2ce4bed362992f667fa296622a4a
Joshua Lamson <jlamson@mymail.mines.edu>
2013-05-04 19:44:02 -0600

    Include list of systems on admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,5 +4,6 @@ class AdminController < ApplicationController
   def index
     return if redirect_if_not_logged_in
     @user = User.find_by_id(session[:user_id])
+    @systems = System.find_all_by_user_id(session[:user_id])
   end
 end
